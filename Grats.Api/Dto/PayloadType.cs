a3481e2eb29c65af0bf8cbe1062f3631fbcf0310
teodoran <teodor@andeelstad.com>
2020-01-05 20:34:22 +0100

    Payload types for view_closed, block_actions and message_actions

--- Grats.Api/Dto/PayloadType.cs
+++ Grats.Api/Dto/PayloadType.cs
@@ -10,5 +10,19 @@ namespace Gratify.Grats.Api.Dto
         public bool IsViewSubmission =>
             Type != null
             && Type == "view_submission";
+
+        public bool IsViewClosed =>
+            Type != null
+            && Type == "view_closed";
+
+        // https://api.slack.com/reference/interaction-payloads/block-actions
+        public bool IsBlockActions =>
+            Type != null
+            && Type == "block_actions";
+
+        // https://api.slack.com/reference/interaction-payloads/actions
+        public bool IsMessageActions =>
+            Type != null
+            && Type == "message_actions";
     }
 }
