94f13af47914a3b6e9b9fefb8c68915077587e8e
teodoran <teodor@andeelstad.com>
2020-04-14 00:07:08 +0200

    Remove unused using statement

--- Gratify.Api/Controllers/CommandsController.cs
+++ Gratify.Api/Controllers/CommandsController.cs
@@ -3,7 +3,6 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Gratify.Api.Database.Entities;
 using Gratify.Api.Dto;
-using Gratify.Api.Modals;
 using Gratify.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
