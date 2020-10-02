3b7c76d0be7d908bfdd13dbf01fba540ce7a9c06
Teodor Ande Elstad <teodor@andeelstad.com>
2020-10-03 00:51:44 +0200

    Check that ISO 20022 library builds and can create credit transfer message

---
+++ Gratify.Api.Test/Iso20022Test.cs
@@ -0,0 +1,50 @@
+using System.Xml;
+using System.Xml.Serialization;
+using Iso20022.Pain;
+using Iso20022.Pain.V3;
+using Xunit;
+
+namespace Gratify.Api.Test
+{
+    public class Iso20022Test
+    {
+        [Fact]
+        public void GenerateXmlTest()
+        {
+            var debitor = new DebitorInformation
+            {
+                Name = "INITECH AS",
+                OrgNr = 123456789,
+                AccountNumber = 11987654321,
+                BankBusinessRegistryCode = "DEVTESTCODE",
+                BankFileTransferId = 1010,
+                CustomerFileTransferId = "SWEDANOR",
+            };
+
+            var transaction = new CreditTransaction(
+                creditorName: "Astri Iversen",
+                creditorAccount: 12345678911,
+                amountNok: 5);
+
+            var initiation = new TransferInitiation(debitor, new CreditTransaction[]
+            {
+                transaction,
+            });
+
+            var document = initiation.Document;
+
+            var serializer = new XmlSerializer(typeof(Document));
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                NewLineChars = "\r\n"
+            };
+
+            // using (var writer = XmlWriter.Create($"/home/teodoran/depot/grats-api/Gratify.Api.Test/output/{initiation.FileName()}", settings))
+            // {
+            //     serializer.Serialize(writer, document);
+            // }
+            Assert.True(true);
+        }
+    }
+}
